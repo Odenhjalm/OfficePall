c30a1e9bde1f81f5b206fe9a6f6111961653b14d
Odenhjalm <Odenhjalm@outlook.com>
2025-08-05 03:25:38 +0200

    Add files via upload
    Create FastAPI server for OfficePall app

---
+++ app/main.py
@@ -0,0 +1,17 @@
+
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from openai_client import query_model
+
+app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/ask", response_class=HTMLResponse)
+async def ask(request: Request, question: str = Form(...)):
+    answer = query_model(question)
+    return templates.TemplateResponse("index.html", {"request": request, "question": question, "answer": answer})
